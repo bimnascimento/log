fe2bc2aec1f456918c57c6621ea88f07d7a974e1
fabio.nascimento <fabio.nascimento@capgemini.com>
2020-06-16 00:05:54 -0300

    _

---
+++ graylog/bkp-mongo.bat
@@ -0,0 +1,3 @@
+@echo off
+docker exec mongodb sh -c "mongodump --host localhost --port 27017 --gzip --archive=/full.gz"
+docker cp mongodb:/full.gz ./mongodb/full.gz
--- graylog/mongodb/bkp-mongo.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-docker exec mongodb sh -c "mongodump --host localhost --port 27017 --gzip --archive=/full.gz"
-docker cp mongodb:/full.gz ./full.gz
